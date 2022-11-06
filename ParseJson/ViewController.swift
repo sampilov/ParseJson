66fb3529412e34656def0049fcf2b16462eb9b5c
Ildar Sampilov <ildar.sampilov@yandex.ru>
2022-11-06 18:48:03 +0600

    Initial Commit

---
+++ ParseJson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParseJson
+//
+//  Created by Цырендылыкова Эржена on 06.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
